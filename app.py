7e05fb653c76480ada1b33df76219dacd3d44c22
Nazmin-4 <nazminspnn462@gmail.com>
2023-02-09 10:18:24 +0530

    added

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask,render_template,request
 from mysql.connector import connect
 con=connect(host='localhost',port=3306,database='mysql' #database name
 ,user='root')
-cur=con.cursor()
-cur.execute("insert into CSE values('nazmin',2),('hema',3)")
-cur.execute("insert into CSE values('sravani',4)")
-con.commit()
+# cur=con.cursor()
+# cur.execute("insert into CSE values('nazmin',2),('hema',3)")
+# cur.execute("insert into CSE values('sravani',4)")
+# con.commit()
 myapp=Flask(__name__)
 @myapp.route("/")
+def regg():
+    return render_template("reg.html")
+
 @myapp.route("/home")
 @myapp.route("/index")
 def hello():
@@ -18,13 +21,39 @@ def about():
 @myapp.route("/name/<string:id>")
 def name(id):
     return "hELLO "+id #dynamic urls
+
 @myapp.route("/myform",methods=["GET","POST"])
 def myform():
     if request.method=="POST":
         user=request.form["name"]
         password=request.form["password"]
-        return render_template("myform.html",user=user)
+        rpass=request.form["rpassword"]
+        if password==rpass:
+            cur=con.cursor()
+            cur.execute("insert into reg values(%s,%s)",(user,rpass))#dynamic values
+            con.commit()
+            # return "success"
+            return render_template("myform.html",msg="successfully registered")
+        else:
+            return "bad credentials"
     else:
         return "get request"
+@myapp.route("/login",methods=["GET","POST"])
+def log():
+    if request.method=="POST": 
+        user=request.form["name"]
+        password=request.form["password"]
+        cur=con.cursor()
+        cur.execute("Select * from reg where name=%s and password=%s",(user,password))
+        a=cur.fetchone()
+        print(a)
+        if a is not None:
+            return render_template("myform.html",msg=user)
+        else:
+            return "user not found"
+    else:
+        return "get request"
+
+
 if __name__=="__main__":
     myapp.run(debug=True)
